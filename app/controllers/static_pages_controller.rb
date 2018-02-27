7ccb034626d87d0ac50ef95e9bb5a12f968d67a6
Unknown <sterx334@gmail.com>
2018-02-27 18:03:37 +0200

    Log in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,5 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
-    
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root 'static_pages#home'
 
-  get 'help'=>'static_pages#help'
-  get 'about'=>'static_pages#about'
-  get 'contact'=>'static_pages#contact'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+
+  get 'signup' => 'user#new'
 
   resources :microposts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'users#index'
 end
